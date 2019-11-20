525ab173146dcee68ee78243c997e58181114d97
Nir Bitton <nir.bitton@here.com>
2019-11-20 21:20:49 +0200

    Initial Commit

---
+++ GameWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameWord
+//
+//  Created by Bitton, Nir on 20/11/2019.
+//  Copyright © 2019 Bitton, Nir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameWordTests/GameWordTests.swift
@@ -0,0 +1,34 @@
+//
+//  GameWordTests.swift
+//  GameWordTests
+//
+//  Created by Bitton, Nir on 20/11/2019.
+//  Copyright © 2019 Bitton, Nir. All rights reserved.
+//
+
+import XCTest
+@testable import GameWord
+
+class GameWordTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
